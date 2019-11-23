b8b32ba19bd703e9574d91625dd0c4e8cbd4a780
Si Thu Hlaing <fumaingqiang@gmail.com>
2019-11-23 17:57:52 +0700

    update projs

---
+++ react-viseo/src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import Button from '@material-ui/core/Button';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <Button variant="contained" color="primary">
+        Hello World
+      </Button>
+    );
+  }
+}
+
+export default App;
